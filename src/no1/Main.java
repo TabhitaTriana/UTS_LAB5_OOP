f61c6473e46340582848867ced83eb21a65d5976
DP-Tabhita-041 <bithhfp@gmail.com>
2024-05-11 17:40:42 +0700

    ketiga

--- src/no1/Main.java
+++ src/no1/Main.java
@@ -23,10 +23,34 @@ public class Main {
 //        mahasiswa.printInfo();
 //        mahasiswa.printInfo("Informasi Mahasiswa:");
 
-          NilaiMhs.printNilai();
 
+        // Input nilai mahasiswa
+        System.out.print("Masukkan Nilai UTS: ");
+        double nilaiUts = scanner.nextDouble();
 
-//
+        System.out.print("Masukkan Nilai UAS: ");
+        double nilaiUas = scanner.nextDouble();
+
+        System.out.print("Masukkan Jumlah Kehadiran: ");
+        int jlhKehadiran = scanner.nextInt();
+
+        System.out.print("Masukkan Nilai Keaktifan: ");
+        int keaktifan = scanner.nextInt();
+
+        // Membuat objek NilaiMhs
+        NilaiMhs nilaiMhs = new NilaiMhs(nilaiUts, nilaiUas, jlhKehadiran, keaktifan);
+
+        // Memanggil method overload 1 untuk mencetak nilai akhir mahasiswa
+        System.out.println("\nHasil dari method overload 1:");
+        nilaiMhs.printNilai();
+
+        // Memanggil method overload 2 untuk mengembalikan nilai akhir mahasiswa
+        double nilaiAkhir = nilaiMhs.printNilai(nilaiUts, nilaiUas, jlhKehadiran, keaktifan);
+        System.out.println("\nHasil dari method overload 2:");
+        System.out.println("Nilai Akhir Mahasiswa: " + nilaiAkhir);
+
+        scanner.close();
+
 //        System.out.println("\nRepresentasi String Objek Mahasiswa:");
 //        System.out.println(mahasiswa.toString());
     }
--- src/no1/NilaiMhs.java
+++ src/no1/NilaiMhs.java
@@ -1,35 +1,45 @@
 package no1;
 
-import java.util.Scanner;
-
-class NilaiMhs {
-    private static String jenisUjian;
-    private static Integer nilaiUts;
-    private static Integer nilaiUas;
-    private double totalNilai;
-
-
-    public static void printNilai() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Masukkan jenis ujian : ");
-        String jenisUjian = scanner.nextLine();
-        System.out.println("Masukkan Nilai UTS : ");
-        String nilaiUts = scanner.nextLine();
-        System.out.println("Masukkan Nilai UAS : ");
-        String nilaiUas = scanner.nextLine();
-        if ("UTS".equals(jenisUjian)) {
-            System.out.println("Nilai UTS: " + nilaiUts);
-        } else if ("UAS".equals(jenisUjian)) {
-            System.out.println("Nilai UAS: " + nilaiUas);
-        } else {
-            System.out.println("Mohon maaf inputan anda salah");
-        }
+public class NilaiMhs {
+    private double nilaiUts;
+    private double nilaiUas;
+    private double hitungNilaiAkhir;
+    private int jlhKehadiran;
+    private int keaktifan;
+    
+    public NilaiMhs(){
+        this.nilaiUts = 0;
+        this.nilaiUas = 0;
+        this.jlhKehadiran = 0;
+        this.keaktifan = 0;
     }
 
-    public double printNilai(String Total) {
-        totalNilai = (nilaiUas + nilaiUts) * 0.5;
-        System.out.println(Total + totalNilai);
-        return totalNilai; // Mengembalikan nilai totalNilai
+    public NilaiMhs(double nilaiUts, double NilaiUas, int jlhKehadiran, int keaktifan){
+         this.nilaiUts = nilaiUts;
+         this.nilaiUas = nilaiUas;
+         this.jlhKehadiran = jlhKehadiran;
+         this.keaktifan = keaktifan;
     }
+
+//    public double printNilai(){
+//        double totalNilai = hitungNilaiAkhir();
+//        System.out.println("Nilai Akhir Mahasiswa : " + totalNilai);
+//        return totalNilai;
+//    }
+
+    public double printNilai(double nilaiUts, double nilaiUas, int jlhKehadiran, int keaktifan){
+          this.nilaiUts = nilaiUts;
+          this.nilaiUas = nilaiUas;
+          this.jlhKehadiran = jlhKehadiran;
+          this.keaktifan = keaktifan;
+
+          return hitungNilaiAkhir();
+    }
+    private double hitungNilaiAkhir(){
+        double nilaiKehadiran = jlhKehadiran * 10;
+        double totalNilai = nilaiUts + nilaiUas + nilaiKehadiran + keaktifan;
+        return totalNilai/4;
+    }
+
 }
 
